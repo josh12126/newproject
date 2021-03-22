e9b876e6eab45ea282f815b2fa220ea6e8bff076
joshua <unyime.josh@gmail.com>
2021-03-22 18:07:31 +0100

    commit

---
+++ data.js
@@ -0,0 +1,83 @@
+const lessons = [
+    {
+        "id": "1",
+        "image":"images/move1.jpg" ,
+        "subject":"English",
+        "location":"Dubai",
+        "price":280,
+        "availableInventory":5  
+    },
+    {
+        "id": "2",
+        "subject":"Mathematics",
+        "location":"Lagos",
+        "price":215,
+        "availableInventory":10,
+        "image":"images/move11.jpg"   
+    },
+    {
+        "id": "3",
+        "subject":"Chemistry",
+        "location":"California",
+        "price":230,
+        "availableInventory":5,
+        "image":"images/move3.jpg"   
+    },
+    {
+        "id": "4",
+        "subject":"Physics",
+        "location":"Texas",
+        "price":110,
+        "availableInventory":5,
+        "image":"images/move4.jpg"   
+    },
+    {
+        "id": "5",
+        "subject":"Nursing",
+        "location":"Australia",
+        "price":150,
+        "availableInventory":5,
+        "image":"images/move5.jpg"   
+    },
+    {
+        "id": "6",
+        "subject":"Medicine",
+        "location":"London",
+        "price":200,
+        "availableInventory":5,
+        "image":"images/move6.jpg"   
+    },
+    {
+        "id": "7",
+        "subject":"Biomedical",
+        "location":"Sweden",
+        "price":110,
+        "availableInventory":5,
+        "image":"images/move7.jpg"   
+    },
+    {
+        "id": "8",
+        "subject":"Programming",
+        "location":"Amsterdam",
+        "price":120,
+        "availableInventory":5,
+        "image":"images/move8.jpg"   
+    },
+    {
+        "id": "9",
+        "subject":"Agricultural Science",
+        "location":"manchester",
+        "price":180,
+        "availableInventory":5,
+        "image":"images/move9.jpg"   
+    },
+    {
+        "id": "10",
+        "subject":"Social Work",
+        "location":"Oxford",
+        "price":100,
+        "availableInventory":5,
+        "image":"images/move10.jpg"   
+    },
+    
+]
